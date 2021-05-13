177f91973a727fbd61a5ef5fc558777f393702ce
Andrés Gallo <andres.gallo@monolegal.co>
2021-05-13 01:03:14 -0500

    Listo para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,36 @@ import 'bulma/css/bulma.css';
 
 function App() {
 
-    const [maxPersons, setMaxPersons] = useState<number>(0);
-    const [maxGroups, setMaxGroups] = useState<number>(0);
-    const [tables, setTables] = useState<number>(0);
-    const [menu, setMenu] = useState<number>(0);
+    const [maxPersons, setMaxPersons] = useState<number>(30);
+    const [maxGroups, setMaxGroups] = useState<number>(25);
+    const [tables, setTables] = useState<number>(16);
+    const [menu, setMenu] = useState<number>(20);
+    const [button, setButton] = useState(false);
+    const [buttons, setButtons] = useState(0);
 
-    const setButtons = (option: number) => {
-        console.log(option)
+    const send_info = async () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "maxGrupos": maxGroups,
+                "maxPorGroup": maxPersons,
+                "mesasNum": tables,
+                "menuNum": menu
+            })
+        };
+        const response = await fetch('https://agente-cliente.herokuapp.com/datos-iniciales', requestOptions)
+        const res = await response.json();
+        console.log(res)
+    }
+
+
+    const changeButton = (option: number) => {
+        if (option === 1) {
+            setButtons(1)
+        } else if (option === 2) {
+            setButtons(2)
+        }
     }
 
     return (
@@ -25,11 +48,12 @@ function App() {
             <div className="tile is-ancestor">
                 <div className="tile is-parent is-vertical">
                     <article className="tile is-child notification is-primary">
-                        <p>En la siguiente aplicación se muestra una simulación de un agente que hace parte de otra aplicación grande de multiagentes e una simulación multiagentes de un restaurante</p>
+                        <p>En la siguiente aplicación se muestra una simulación de un agente que hace parte de otra
+                            aplicación grande de multiagentes e una simulación multiagentes de un restaurante</p>
                     </article>
                     <div className="container tile is-parent content is-text is-multiline">
                         <article className="tile is-child notification is-success">
-                            <h1 className="subtitle">Los tiempos para la simulación son los siguientes:</h1>
+                            <h1 className="subtitle">Los valores para la simulación son los siguientes:</h1>
                             <ul>
                                 <li>- Máximo de personas por grupos: {maxPersons}</li>
                                 <li>- Máximo de grupos: {maxGroups}</li>
@@ -38,17 +62,20 @@ function App() {
                             </ul>
                         </article>
                     </div>
-                    <div>
+                    <div hidden={buttons !== 0}>
                         <div className="tile is-child buttons">
-                            <button className="button is-primary">Iniciar
+                            <button className="button is-primary" onClick={() => {
+                                send_info();
+                                changeButton(1)
+                            }}>Iniciar
                             </button>
-                            <button className="button is-link">Modificar Tiempos
+                            <button className="button is-link" onClick={() => changeButton(2)}>Modificar Tiempos
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="container">
+            <div className="container" hidden={buttons !== 2}>
                 <h1 className="title">Modificar valores de funcionamiento del agente </h1>
                 <div className="field is-horizontal">
                     <div className="field-label is-normal">
@@ -115,7 +142,7 @@ function App() {
                                 <button className="button is-primary" onClick={() => {
                                     setButtons(0)
                                 }}>
-                                    Guardar tiempos
+                                    Guardar valores
                                 </button>
                             </div>
                         </div>
